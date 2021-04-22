c6af77d018d22e466436c01a793e07040d2f6e62
Thiago Negreiros <negreiros.t@gmail.com>
2021-04-22 14:02:57 -0300

    NLW | Aula 02 Criando estrutura com typeorm

---
+++ src/repositories/SettingsRepository.ts
@@ -0,0 +1,7 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Setting } from "../entities/Setting";
+
+@EntityRepository(Setting)
+export class SettingsRepository extends Repository<Setting> {
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
-
+import './database'
+import { router } from './routes'
 
 const app = express();
+
+app.use(express.json())
+app.use(router);
 
 app.listen(3333, () => console.log("Server running on port 3333"))
